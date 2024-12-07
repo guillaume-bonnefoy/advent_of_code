72cb52d92efe25b7868442508faa3b1c22e3a36e
Guillaume <guillaume.bonnefoy@patrowl.io>
2024-12-07 19:42:31 +0100

    feat: part 2

--- 2024_7/main.ts
+++ 2024_7/main.ts
@@ -1,3 +1,5 @@
+console.time('Execution time')
+
 const testInput = `
 190: 10 19
 3267: 81 40 27
@@ -11,7 +13,7 @@ const testInput = `
 `
 
 const fullInput = await Deno.readTextFile('input.txt')
-const inputString = testInput.trim()
+const inputString = fullInput.trim()
 
 console.log(`
 —————————————————————————————————————————————————————————————————————
@@ -22,7 +24,6 @@ const rawLines = inputString.trim().split('\n')
 type Line = {
   expectedResult: number
   numbers: number[]
-  validCombinations: number
 }
 
 const formattedLines: Line[] = []
@@ -30,7 +31,7 @@ const formattedLines: Line[] = []
 for (const rawLine of rawLines) {
   const expectedResult = parseInt(rawLine.split(': ')[0])
   const numbers = rawLine.split(': ')[1].split(' ').map(Number)
-  formattedLines.push({ expectedResult, numbers, validCombinations: 0 })
+  formattedLines.push({ expectedResult, numbers })
 }
 
 console.log(formattedLines)
@@ -40,15 +41,18 @@ console.log(`
 ‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿୨   COMPUTING   ୧‿︵‿︵‿︵‿︵‿︵‿︵‿︵‿︵
 —————————————————————————————————————————————————————————————————————`)
 
-function computeValidCombinations(line: Line, operation: 'add' | 'multiply') {
+function computeValidCombinations(line: Line, operation: 'add' | 'multiply' | 'concat') {
   const lineNumbers = structuredClone(line.numbers)
   while (lineNumbers.length > 1) {
     const a = lineNumbers.shift() as number
     const b = lineNumbers.shift() as number
-    let result = operation === 'add' ? a + b : a * b
+
+    let result = 0
+    if (operation === 'add') result = a + b
+    if (operation === 'multiply') result = a * b
+    if (operation === 'concat') result = Number(`${a}${b}`)
 
     if (result === line.expectedResult && !lineNumbers.length) {
-      line.validCombinations++
       return true
     }
 
@@ -59,31 +63,38 @@ function computeValidCombinations(line: Line, operation: 'add' | 'multiply') {
     const validAdd = computeValidCombinations({
       numbers: [result, ...lineNumbers],
       expectedResult: line.expectedResult,
-      validCombinations: line.validCombinations,
     }, 'add')
 
     const validMultiply = computeValidCombinations({
       numbers: [result, ...lineNumbers],
       expectedResult: line.expectedResult,
-      validCombinations: line.validCombinations,
     }, 'multiply')
 
-    if (validAdd || validMultiply) return true
+    const validConcat = computeValidCombinations({
+      numbers: [result, ...lineNumbers],
+      expectedResult: line.expectedResult,
+    }, 'concat')
+
+    if (validAdd || validMultiply || validConcat) return true
   }
 
   return false
 }
 
 let sumOfValidCombinations = 0
+let numberOfValidCombinations = 0
 
 formattedLines.forEach((line) => {
-  // console.log(line.expectedResult, '----------------')
   const isValidAdd = computeValidCombinations(line, 'add')
   const isValidMultiply = computeValidCombinations(line, 'multiply')
-  const isEitherValid = isValidAdd || isValidMultiply
-  // console.log(isEitherValid ? '🎉' : '❌')
-  // console.log('\n')
+  const isValidConcat = computeValidCombinations(line, 'concat')
+  const isEitherValid = isValidAdd || isValidMultiply || isValidConcat
   isEitherValid && (sumOfValidCombinations += line.expectedResult)
+  isEitherValid && numberOfValidCombinations++
+  // console.log(isEitherValid ? '✅' : '❌', line.expectedResult)
+
+  process.stdout.write(`\rProcessing line ${formattedLines.indexOf(line) + 1}/${formattedLines.length} | ${numberOfValidCombinations} valid combinations`)
 })
 
 console.log('🚨 Sum of valid combinations:', sumOfValidCombinations)
+console.timeEnd('Execution time')
